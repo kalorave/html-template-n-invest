c23c33ba3910ba43f523321237b50c4c510ef9c2
JoeBair <workpage.maxim@gmail.com>
2020-07-26 01:21:22 +0300

    add block

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 new fullpage('#fullpage');
+
+(function() {
+  let mapElems = document.querySelectorAll('#svg-map path');
+
+  let x = 5;
+
+  mapElems.forEach((path, i) => {
+    path.onmouseenter = () => {
+      console.log(`${Math.floor(i / x) + 1}; ${(i % x) + 1}`);
+    }
+    path.onmouseleave = () => {
+
+    }
+  });
+
+})();
